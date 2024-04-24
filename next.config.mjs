56e2c2c8f5317aae12594b982abb65991f002c89
Shaon Bhattacharjee <bshaon19@gmail.com>
2024-04-24 21:39:41 +0530

    v:4.2.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /**@type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   compiler: {
-    reactStrictMode: true,
     styledComponents: true,
   },
 };
+
 export default nextConfig;
